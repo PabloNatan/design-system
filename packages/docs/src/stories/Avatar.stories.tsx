f34c66b637fac55d06be6aace7c26efeb9874ed8
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-02-25 07:43:04 -0300

    define arg types

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/pablonatan.png',
     alt: 'Pablo Natan',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
